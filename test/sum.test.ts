d6074a59595dcc6c1c1a7bb3a1477d082becab03
ismail-codinglab <48755937+ismail-codinglab@users.noreply.github.com>
2020-04-10 16:57:41 +0200

    feat(sum): increased test cases with unique and negative values

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,7 +1,18 @@
 import { sum } from '../src'
 
 describe('sum', () => {
-  it('should sum two values', () => {
-    expect(sum(1, 1)).toEqual(2)
-  })
+  
+    it('should sum two unique values', () => {
+      expect(sum(3, 5)).toEqual(8)
+    })
+  
+    it('should sum a negative with a positive value', () => {
+      expect(sum(-3, 5)).toEqual(2)
+      expect(sum(3, -5)).toEqual(-2)
+    })
+
+    it('should sum two negative values', () => {
+      expect(sum(-3, -5)).toEqual(-8)
+    })
+
 })
