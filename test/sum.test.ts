b61ac6dff5abf84cb0f99aee87ceb8ec0062ed7d
Daphne Smit <daphnesmit1986@gmail.com>
2020-04-11 11:01:49 +0200

    fix(linting): fix linting

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,18 +1,16 @@
 import { sum } from '../src'
 
 describe('sum', () => {
-  
-    it('should sum two unique values', () => {
-      expect(sum(3, 5)).toEqual(8)
-    })
-  
-    it('should sum a negative with a positive value', () => {
-      expect(sum(-3, 5)).toEqual(2)
-      expect(sum(3, -5)).toEqual(-2)
-    })
+  it('should sum two unique values', () => {
+    expect(sum(3, 5)).toEqual(8)
+  })
 
-    it('should sum two negative values', () => {
-      expect(sum(-3, -5)).toEqual(-8)
-    })
+  it('should sum a negative with a positive value', () => {
+    expect(sum(-3, 5)).toEqual(2)
+    expect(sum(3, -5)).toEqual(-2)
+  })
 
+  it('should sum two negative values', () => {
+    expect(sum(-3, -5)).toEqual(-8)
+  })
 })
